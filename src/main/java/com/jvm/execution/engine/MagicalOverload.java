be41201c910e2e88db745dd8ee40d78b9ff8f90d
LightMingMing <1849901944@qq.com>
2019-01-13 21:34:25 +0800

    Method overload: match priority

---
+++ src/main/java/com/jvm/execution/engine/MagicalOverload.java
@@ -0,0 +1,57 @@
+package com.jvm.execution.engine;
+
+import java.io.Serializable;
+
+/**
+ * 重载
+ *
+ * @author LightMingMing
+ */
+public class MagicalOverload {
+
+    // char -> int -> long -> float -> double -> Character -> Serializable -> Object -> char... -> int...  -> ... -> Object...
+
+    public static void main(String[] args) {
+
+        System.out.println(new MagicalOverload().hello('c'));
+
+    }
+
+    public String hello(char c) {
+        return "Hello, char";
+    }
+
+    public String hello(int c) {
+        return "Hello, int";
+    }
+
+    public String hello(long c) {
+        return "Hello, long";
+    }
+
+    public String hello(double c) {
+        return "Hello, double";
+    }
+
+    public String hello(Serializable c) {
+        return "Hello, Serializable";
+    }
+
+    public String hello(Object c) {
+        return "Hello, Object";
+    }
+
+    public String hello(char... c) {
+        return "Hello, char...";
+    }
+
+    public String hello(int... c) {
+        return "Hello, int...";
+    }
+
+    // ...
+
+    public String hello(Object... c) {
+        return "Hello, object...";
+    }
+}
