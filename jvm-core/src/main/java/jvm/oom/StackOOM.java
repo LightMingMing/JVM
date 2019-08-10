eacf5e863f12ba8cb702820ca42ac213f42d7b37
LightMingMing <1849901944@qq.com>
2019-08-10 13:30:38 +0800

    Avoid CPU busyness

--- jvm-core/src/main/java/jvm/oom/StackOOM.java
+++ jvm-core/src/main/java/jvm/oom/StackOOM.java
@@ -2,27 +2,46 @@ package jvm.oom;
 
 /**
  * 创建线程导致内存溢出
- * VM Args: -Xss2m
+ * VM Args: -Xss2m -Xms10m -Xmx10m
  *
  * @author LightMingMing
  */
 public class StackOOM {
 
     private static void dontStop() {
+        // 书上案例的话, 只有while(true){}, 会使CPU飙到最高, 电脑直接卡死.
         while (true) {
-        }
-    }
-
-    public void stackLeakByCreateThread() {
-        while (true) {
-            new Thread(StackOOM::dontStop).start();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // ignore
+            }
+            double i = Math.PI;
+            while (i < 1000000.05) {
+                i += Math.PI;
+            }
         }
     }
 
     public static void main(String[] args) {
         new StackOOM().stackLeakByCreateThread();
     }
+
+    public void stackLeakByCreateThread() {
+        while (true) {
+            try {
+                new Thread(StackOOM::dontStop).start();
+            } catch (Error e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
 }
 /*
-难受，运行不出来，电脑直接卡死...
+java.lang.OutOfMemoryError: unable to create new native thread
+	at java.lang.Thread.start0(Native Method)
+	at java.lang.Thread.start(Thread.java:717)
+	at jvm.oom.StackOOM.stackLeakByCreateThread(StackOOM.java:32)
+	at jvm.oom.StackOOM.main(StackOOM.java:26)
  */
