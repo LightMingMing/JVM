1f48dfd96ee26eaeaf425d88902b9a7b5aeed9f6
LightMingMing <1849901944@qq.com>
2020-06-19 19:41:41 +0800

    Introduce gc type detector

---
+++ jvm-core/src/main/java/jvm/gc/GarbageCollectorTypeDetector.java
@@ -0,0 +1,65 @@
+package jvm.gc;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryManagerMXBean;
+
+/**
+ * 垃圾收集器探测
+ * <p>
+ * JDK 1.8 可能组合
+ * <li>PS Scavenge + PS MarkSweep</li>
+ * <li>ParNew + ConcurrentMarkSweep</li>
+ * <li>G1 Young Generation + G1 Old Generation</li>
+ * <li>ParNew + MarkSweepCompact (不推荐)</li>
+ *
+ * @author LightMingMing
+ */
+public class GarbageCollectorTypeDetector {
+
+    public static void main(String[] args) {
+        new GarbageCollectorTypeDetector().printGarbageCollectorType();
+    }
+
+    private void printGarbageCollectorType() {
+        ManagementFactory.getGarbageCollectorMXBeans().stream().map(MemoryManagerMXBean::getName).forEach(System.out::println);
+    }
+}
+
+// TODO JDK 1.7
+
+// JDK 1.8
+/*
+PS Scavenge
+PS MarkSweep
+ */
+
+/*
+-XX:+UseParallelGC
+PS Scavenge
+PS MarkSweep
+ */
+
+/*
+-XX:+UseParallelOldGC
+PS Scavenge
+PS MarkSweep
+ */
+
+/*
+-XX:+UseParNewGC
+ParNew
+MarkSweepCompact
+Java HotSpot(TM) 64-Bit Server VM warning: Using the ParNew young collector with the Serial old collector is deprecated and will likely be removed in a future release
+ */
+
+/*
+-XX:+UseConcMarkSweepGC
+ParNew
+ConcurrentMarkSweep
+ */
+
+/*
+-XX:+UseG1GC
+G1 Young Generation
+G1 Old Generation
+ */
